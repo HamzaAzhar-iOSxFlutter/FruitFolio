abc2416565b89b236bd35a855ffbb20e1832a3fb
hamzaazharxiOS <hamzaazharcs@gmail.com>
2023-10-10 23:19:10 -0400

    Starter Project

---
+++ FruitFolio/FruitFolio/FruitFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitFolioApp.swift
+//  FruitFolio
+//
+//  Created by Hamza Azhar on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
