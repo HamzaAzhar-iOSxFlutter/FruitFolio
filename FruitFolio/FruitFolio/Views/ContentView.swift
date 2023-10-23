faec8754cd2429b927083683caa7538d097f7bc2
daniyal392 <134169837+daniyal392@users.noreply.github.com>
2023-10-23 23:55:18 +0200

    Completed the list view for the list of fruits.
    The detail screen for every fruit is pending.

--- FruitFolio/FruitFolio/Views/ContentView.swift
+++ FruitFolio/FruitFolio/Views/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var fruits: [Fruit] = fruitsData
+    @State private var isShowingSettings: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List {
+                ForEach(self.fruits.shuffled()) { item in
+//                    NavigationLink(destination: FruitDetailView(fruit: item)) {
+//                        FruitRowView(fruit: item)
+//                            .padding(.vertical, 4)
+//                    }
+                }
+            }//: LIST
+            .navigationTitle("Fruits")
+            .navigationBarTitleDisplayMode(.large)
+            /*
+             The commented code below, is basically for future improvements.
+             */
+//            .navigationBarItems(
+//                trailing:
+//                    Button(action: {
+//                        self.isShowingSettings = true
+//                    }) {
+//                        Image(systemName: "slider.horizontal.3")
+//                    }//: Button
+//                    .sheet(isPresented: $isShowingSettings) {
+//                        SettingsView()
+//                    }
+//            )
+        }//: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
